8318af1ec006b74ccadea6819ff0aa7b9e5773c3
Patrick <mail@shyxormz.net>
2018-01-01 23:17:41 +0100

    Add GetAudioDeviceName()

---
+++ audio.go
@@ -0,0 +1,17 @@
+package sdl
+
+// #cgo windows LDFLAGS: -lSDL2
+// #include <SDL2/SDL.h>
+import "C"
+
+func GetAudioDeviceName(device int, isCapture bool) (string, error) {
+	var capture int
+	if isCapture {
+		capture = 1
+	}
+	ptr := C.SDL_GetAudioDeviceName(C.int(device), C.int(capture))
+	if ptr == nil {
+		return "", GetError()
+	}
+	return C.GoString(ptr), nil
+}
