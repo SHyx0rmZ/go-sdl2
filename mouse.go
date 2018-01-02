fb00df4c9d2d77557b402d33b6b9e79eeebc1629
Patrick <mail@shyxormz.net>
2018-01-02 01:25:50 +0100

    Add (MouseButtons).String()

--- mouse.go
+++ mouse.go
@@ -7,3 +7,23 @@ type MouseButtons struct {
 	X1     bool
 	X2     bool
 }
+
+func (b MouseButtons) String() string {
+	s := []byte{'-', '-', '-', '-', '-'}
+	if b.Left {
+		s[0] = 'L'
+	}
+	if b.Middle {
+		s[1] = 'M'
+	}
+	if b.Right {
+		s[2] = 'R'
+	}
+	if b.X1 {
+		s[3] = 'X'
+	}
+	if b.X2 {
+		s[4] = 'X'
+	}
+	return string(s)
+}
