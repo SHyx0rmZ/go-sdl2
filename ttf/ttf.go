5fd5270ef8ad6bcbe4ba7d7c82f27f5c06163e77
Patrick Pokatilo <mail@shyxormz.net>
2019-08-26 22:53:14 +0200

    Also link ttf against SDL2

--- ttf/ttf.go
+++ ttf/ttf.go
@@ -1,6 +1,6 @@
 package ttf
 
-// #cgo windows LDFLAGS: -lSDL2_ttf
+// #cgo windows LDFLAGS: -lSDL2_ttf -lSDL2
 // #cgo linux freebsd darwin pkg-config: SDL2_ttf
 // #include <SDL2/SDL_ttf.h>
 import "C"
