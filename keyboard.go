008d5e147bfe4a3b3c750a6331c5ece5ac117b22
Patrick Pokatilo <mail@shyxormz.net>
2022-08-27 05:43:43 +0200

    Add GetKeyboardState

--- keyboard.go
+++ keyboard.go
@@ -2,9 +2,20 @@ package sdl
 
 // #include <SDL2/SDL.h>
 import "C"
+import (
+	"unsafe"
+)
 
 type KeySymbol struct {
 	Scancode  ScanCode
 	Keycode   KeyCode
 	Modifiers KeyModifiers
+}
+
+func GetKeyboardState() []uint8 {
+	var numkeys int32
+	ptr := C.SDL_GetKeyboardState((*C.int)(unsafe.Pointer(&numkeys)))
+	ary := make([]uint8, numkeys)
+	copy(ary, unsafe.Slice((*uint8)(unsafe.Pointer(ptr)), numkeys))
+	return ary
 }
