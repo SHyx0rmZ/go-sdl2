7d47e227e130f4def91aba6e615d7613bc75f904
Patrick Pokatilo <mail@shyxormz.net>
2022-12-01 03:50:49 +0100

    Use stdatomic to communicate with event loop

--- events.c
+++ events.c
@@ -1,36 +1,28 @@
 #include "events.h"
 #include <stdio.h>
+#include <stdatomic.h>
 #include "_cgo_export.h"
-
-volatile Uint32 collectEventsQuit;
-
-int catchAppTerminating(void *userdata, SDL_Event *event) {
-    if (event->type == SDL_APP_TERMINATING) {
-        collectEventsQuit = 1;
-    }
-    return 1;
-}
 
 void collectEvents(volatile Uint32 *collectEventsQuit) {
   SDL_Event event;
-  while(*collectEventsQuit == 0) {
+  while(atomic_load(collectEventsQuit) == 0) {
 
     SDL_PumpEvents();
 
 #if 0
     switch(SDL_PeepEvents(&event, 1, SDL_GETEVENT, SDL_FIRSTEVENT, SDL_LASTEVENT)) {
     case 1:
-      if (*collectEventsQuit == 0) {
+      if (atomic_load(collectEventsQuit) == 0) {
         propevent(&event);
       }
     }
 #else
     if (SDL_WaitEventTimeout(&event, 10) == 1) {
-      if (*collectEventsQuit == 0) {
+      if (atomic_load(collectEventsQuit) == 0) {
         propevent(&event);
       }
     }
 #endif
   }
-  *collectEventsQuit = 2;
+  atomic_fetch_add(collectEventsQuit, 1);
 }
